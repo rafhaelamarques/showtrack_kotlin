168e9177d5ca6a95ae1ded68cf5e72f851f1caaf
Rafhaela Marques <rafaelamarques_z@hotmail.com>
2022-06-05 15:26:39 -0300

    inicio do projeto

---
+++ ShowTrack/app/src/main/java/com/cotemig/showtrack/ui/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.cotemig.showtrack.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cotemig.showtrack.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
