a7e3bf23417f9fb5eb6647f7eb7aafba5b1c2ece
cristianebatista <cristianebatista@dcc.ufmg.br>
2022-06-05 21:38:56 -0300

    Layout cadastro

---
+++ ShowTrack/app/src/main/java/com/cotemig/showtrack/ui/activities/CadastroActivity.kt
@@ -0,0 +1,11 @@
+package com.cotemig.showtrack.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
